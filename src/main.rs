362457fb54361e9b90a32556d68a10329b43af3d
Christian Oudard <ChristianOudard@pm.me>
2024-09-20 13:58:06 -0600

    I-tetromino not rendering due to color zero matching the background.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use tetromino_console::{move_shape, normalize, point, rotate_shape, tetrominoes, BoxGrid, Point};
+use tetromino_console::{move_shape, point, tetrominoes, BoxGrid};
 
 fn main() {
     let tets = tetrominoes();
     let mut grid = BoxGrid::new();
     for (i, t) in tets.iter().enumerate() {
         let i = i as i32;
-        let t = move_shape(t, &point(i * 5, 0));
-        grid.set_shape(&t, i as u8);
+        let t = move_shape(t, &point(i * 4, 0));
+        let color = (i + 1) as u8;
+        grid.set_shape(&t, color);
     }
+    print!("{}", grid.render());
 }
-
-
